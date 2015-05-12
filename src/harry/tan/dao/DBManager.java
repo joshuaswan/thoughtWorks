fa398f2226796b746f6b8b7b1c52159d06927829
harry <harrytan@aaxischina.com>
2015-05-12 18:27:52 +0800

    submit some files

--- src/harry/tan/dao/DBManager.java
+++ src/harry/tan/dao/DBManager.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -21,11 +25,11 @@ public class DBManager {
 
     /**
      * 
-     * <method description> 执行查询
+     * <method description> 执行插入
      * 
      * @return
      */
-    public Object executeQuery(String sql, Object... objects) {
+    public Object executeUpdate(String sql, Object... objects) {
         ResultSet rs = null;
         Object obj = null;
         try {
@@ -36,20 +40,81 @@ public class DBManager {
             }
 
             for (int i = 0; i < objects.length; i++) {
-                preparedStatement.setObject(i+1, objects[i]);
-            }
-            
-            boolean b = preparedStatement.execute();
-            rs = preparedStatement.getGeneratedKeys();
-            if (rs.next()) {
-                obj = rs.getObject(1);
+                preparedStatement.setObject(i + 1, objects[i]);
             }
 
-            return obj;
+            boolean bool = preparedStatement.execute();
+            if (bool) {
+                rs = preparedStatement.getGeneratedKeys();
+                if (rs.next()) {
+                    obj = rs.getObject(1);
+                }
+
+                return obj;
+            }
+
+            return null;
         } catch (SQLException e) {
             System.out.println(e);
         }
-
+        finally{
+            PoolUtil.release(conn,preparedStatement,rs);
+        }
+        
         return null;
     }
+    
+    /**
+     * 
+    * <method description>
+    *   主要执行查询操作
+    * @param sql
+    * @param objects
+    * @return
+     */
+    public List<Map<String,Object>> executeQuery(String sql, Object... objects){
+        ResultSet rs = null;
+        List<Map<String,Object>> listMap = null;
+        try {
+            conn = PoolUtil.getConnection();
+            preparedStatement = conn.prepareStatement(sql);
+            if(preparedStatement.getMetaData().getColumnCount() != objects.length){
+                throw new RuntimeException("参数个数不匹配");
+            }
+            
+            for (int i = 0; i < objects.length; i++) {
+                preparedStatement.setObject(i + 1, objects[i]);
+            }
+            
+            rs = preparedStatement.executeQuery();
+            if(null != rs){
+                
+                // 有必要去实例化listMap
+                listMap = new ArrayList<Map<String,Object>>();
+                while(rs.next()){
+                    for(int i=0;i<rs.getMetaData().getColumnCount();i++){
+                        Map<String,Object> map = new HashMap<String, Object>();
+                        final String columName = rs.getMetaData().getColumnName(i+1);
+                        final Object columValue = rs.getObject(columName);
+                        map.put(columName, columValue);
+                        listMap.add(map);
+                    }
+                }
+                
+                return listMap;
+            }
+            
+            // rs为null，直接返回null
+            return null;
+            
+        } catch (SQLException e) {
+           System.out.println("get the connection is failed");
+        }
+        finally{
+            PoolUtil.release(conn,preparedStatement,rs);
+        }
+        
+        return null;
+    }
+
 }
