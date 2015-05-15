9dd8fc8a8a862fb2341bc46eb728094a27808cf0
harry <harrytan@aaxischina.com>
2015-05-15 17:39:01 +0800

    url address

--- src/harry/tan/test/TestMain.java
+++ src/harry/tan/test/TestMain.java
@@ -4,6 +4,12 @@ import harry.tan.serviceImpl.TrainsServiceImpl;
 
 public class TestMain {
 
+    /**
+     * http://blog.csdn.net/javaman_chen/article/details/8254309
+    * <method description>
+    *
+    * @param args
+     */
 	public static void main(String[] args) {
 		
 		TrainsServiceImpl m = new TrainsServiceImpl();
